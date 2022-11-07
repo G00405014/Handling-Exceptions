7945bf29fad220bf8bea4e2b33b7b45687a0e27b
G00405014 <G00405014@atu.ie>
2022-11-07 10:11:10 +0000

    While loop- continue using application

--- src/main/java/week7/UsingTryCatch.java
+++ src/main/java/week7/UsingTryCatch.java
@@ -3,22 +3,28 @@ package week7;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+//Name: Emeka Adimora
+//Date: 07/11/22
+//Student id: G00405014
+
 public class UsingTryCatch {
     public static void main(String[] args) {
-        try {
-            System.out.println("Please enter a number between 0-9 :");
-            Scanner myscan = new Scanner(System.in);
-            int myNum = myscan.nextInt();
+        int num = 1;
+        while(num > 0) {
+            try {
+                System.out.println("Please enter a number between 0-9 :");
+                Scanner myscan = new Scanner(System.in);
+                int myNum = myscan.nextInt();
 
-            if (myNum <= 9 && myNum > 0) {
-                System.out.println("You entered" + myNum);
-            } else {
-                System.out.println("Not valid number");
+                if (myNum <= 9 && myNum > 0) {
+                    System.out.println("You entered" + myNum);
+                } else {
+                    System.out.println("Not valid number");
+                }
+            //Catching error
+            } catch (InputMismatchException ime) {
+                System.out.println("Caught exception ");
             }
         }
-    catch(InputMismatchException e)
-    {
-        System.out.println("Error received ");
-    }
 }
 }
